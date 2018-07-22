06eceafb09597fe70ac94d161ab50af70b7d0ab0
Ledum <ledumibaakee@gmail.com>
2018-07-22 22:16:37 +0100

    Initial

--- ecom/settings.py
+++ ecom/settings.py
@@ -85,11 +85,11 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 
 
